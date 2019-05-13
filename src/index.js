bdc70873bf4857e5e2a7dfb781b1b34bb7c48872
luan henrique <luan.henriqueepp@gmail.com>
2019-05-13 14:10:22 -0300

    Criação do primeiro component utiliando JSX

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import PrimeiroComponent from './components/PrimeiroComponent';
 
 const elemento = document.getElementById('root');
+ReactDOM.render(
+    <div>
+        <PrimeiroComponent/>
+    </div>
+, elemento);
 
-ReactDOM.render(<h1>Olá Mundo</h1>, elemento);
-
+// ReactDOM.render(<h1>Olá Mundo</h1>, elemento);
+/*
+ReactDOM.render(
+    <ul>
+        <li>1) Luan</li>
+        <li>1) João</li>
+        <li>1) Mãe do João</li>
+    </ul>
+, elemento);*/
 
