f8eda2c77dcebcd3ff777ee781ed644f42358077
luan henrique <luan.henriqueepp@gmail.com>
2019-08-08 13:04:30 -0300

    Dando continuidade ao curso de react, vendo sobre o conceito de multiplos components conversando entre sí

--- src/components/SegundoComponent.jsx
+++ src/components/SegundoComponent.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const CompA= props => <h1>Primeiro diz: {props.valor}</h1>;
+// export
+const CompA= props =>
+    <h1>Primeiro diz: {props.valor}</h1>;
 
-const CompB = props => <h1>Segundo diz: {props.valor}</h1>;
+// export
+const CompB = props =>
+    <h1>Segundo diz: {props.valor}</h1>;
 
 export {CompA, CompB}
+export default CompA
+
+/*
+export default function () {
+    return <p>teste</p>
+}
+*/
